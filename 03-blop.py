1d3f7e80b8141273116c0e9d8f6bb5e9d08d6d16
Zachary Lentz <zlentz@slac.stanford.edu>
2024-08-14 11:12:45 -0700

    ENH: put in some stock ipm xpos optimizer

--- 03-blop.py
+++ 03-blop.py
@@ -6,21 +6,45 @@ ip = get_ipython()
 ip.run_line_magic("run", "-i $prepare_re_env.__file__ --db-type=temp")
 bec.disable_plots()
 
+# Center position most commonly close to the trajectory
+MIRROR_NOMINAL = -535
+# Search +- the delta from the nominal
+SEARCH_DELTA = 5
+# WAVE8 goal positions
+DG1_WAVE8_XPOS = 0
+# DG2 unused for now
+DG2_WAVE8_XPOS = 0
+# How close are we trying to get to the target?
+WAVE8_TOL = 0.1
+# If the WAVE8 poxition is above this value or below negative this value, don't trust the value
+WAVE8_MAX_VALUE = 10
+
 dofs = [
-    DOF(description="MFX Mirror", device=mirror.pitch, search_domain=(839, 840)),
+    DOF(
+        description="MFX Mirror",
+        device=mirror.pitch,
+        search_domain=(MIRROR_NOMINAL - SEARCH_DELTA, MIRROR_NOMINAL + SEARCH_DELTA),
+    ),
 ]
 
 objectives = [
-    Objective(name="mfx_dg1_wave8_sum", target="max")
+    Objective(
+        name="mfx_dg1_wave8_xpos",
+        target=(DG1_WAVE8_XPOS - WAVE8_TOL, DG1_WAVE8_XPOS + WAVE8_TOL),
+        trust_domain=(-1 * WAVE8_MAX_VALUE, WAVE8_MAX_VALUE),
+    ),
 ]
 
 agent = Agent(
     dofs=dofs,
     objectives=objectives,
-    detectors=[dg1_wave8.sum],
+    detectors=[dg1_wave8.xpos],
     verbose=True,
     db=db,
     tolerate_acquisition_errors=False,
     enforce_all_objectives_valid=True,
     train_every=3,
 )
+# RE(agent.learn("qei", n=20))
+# Options for optimizer: https://github.com/NSLS-II/blop/blob/main/src/blop/bayesian/acquisition/config.yml
+# Signature: https://github.com/NSLS-II/blop/blob/76ddd5a64db4a7eddd99c5d1bd3a92823b2ccb23/src/blop/agent.py#L388
